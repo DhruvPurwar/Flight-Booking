900ac94450faf6eb22103af1883fe4a12c5ebe6a
Dhruv <dhruvpurwar15@gmail.com>
2023-06-12 11:55:43 +0530

    Added booking payment logic

--- services/booking-service.js
+++ services/booking-service.js
@@ -4,6 +4,8 @@ const { BookingRepository } = require("../repositories");
 const db = require("../models");
 const { ServerConfig } = require("../config");
 const AppError = require("../utils/errors/app-error");
+const { Enums } = require("../utils/common");
+const { BOOKED, CANCELLED, PENDING, INITIATED } = Enums.BOOKING_STATUS;
 
 const bookingRepository = new BookingRepository();
 
@@ -38,4 +40,59 @@ async function createBooking(data) {
   }
 }
 
-module.exports = { createBooking };
+async function makePayment(data) {
+  const transaction = await db.sequelize.transaction();
+  console.log(data);
+  try {
+    const bookingDetails = await bookingRepository.get(
+      data.bookingId,
+      transaction
+    );
+    if (bookingDetails.status == CANCELLED) {
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+
+    const bookingTime = new Date(bookingDetails.createdAt);
+    const currentTime = new Date();
+    if (currentTime - bookingTime > 300000) {
+      await bookingRepository.update(
+        data.bookingId,
+        {
+          status: CANCELLED,
+        },
+        transaction
+      );
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+    if (bookingDetails.totalCost != data.totalCost) {
+      console.log(typeof bookingDetails.totalCost);
+      console.log(typeof data.totalCost);
+      throw new AppError(
+        "Amount of payment doesnt match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+
+    if (bookingDetails.userId != data.userId) {
+      throw new AppError(
+        "User of corresponding to this booking doesnt match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+
+    // we assume payment is success
+    await bookingRepository.update(
+      data.bookingId,
+      {
+        status: BOOKED,
+      },
+      transaction
+    );
+    await transaction.commit(); //sadd return response
+  } catch (error) {
+    await transaction.rollback(); // manually rollback
+    throw error;
+  }
+}
+
+module.exports = { createBooking, makePayment };
